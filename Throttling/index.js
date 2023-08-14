c54ec3a63805692ead425e09922a35be5100acb4
unknown <anji.gd17@gmail.com>
2023-08-14 23:54:34 +0530

    debouncing

--- Throttling/index.js
+++ Throttling/index.js
@@ -1,4 +1,5 @@
 const btn=document.getElementById('btn');
+const txt=document.getElementById('text');
 
 function troattling(func,delay){
     let id=false
@@ -13,3 +14,20 @@ function troattling(func,delay){
     }
 }
 
+function debouncing(func,delay){
+    let id
+    return function(){
+       clearTimeout(id)
+       id=setTimeout(()=>{
+         func();
+        },delay)
+    }
+}
+
+function generateRandom(){
+    let num= Math.floor(Math.random() * (50 - 1 + 1))
+    txt.innerText=num
+}
+
+// btn.addEventListener('click',troattling(generateRandom,3000))
+btn.addEventListener('click',debouncing(generateRandom,3000))
