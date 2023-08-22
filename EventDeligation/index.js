3047773bcfe3901c7841a485a4346d60b77761ab
unknown <anji.gd17@gmail.com>
2023-08-22 18:40:09 +0530

    Event Deligation

--- EventDeligation/index.js
+++ EventDeligation/index.js
@@ -6,3 +6,14 @@ itemList.addEventListener("click", (e) => {
     document.getElementById("app").innerHTML=`Clicked on: ${e.target.textContent}`;
   }
 });
+
+const mainMenu = document.getElementById("mainMenu");
+
+mainMenu.addEventListener("click", (event) => {
+  if (event.target.classList.contains("dropdown-toggle")) {
+    const dropdown = event.target.querySelector(".dropdown");
+    if (dropdown) {
+      dropdown.classList.toggle("show");
+    }
+  }
+});
